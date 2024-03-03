6a04511b9bc85655b0ffaacdad2704e0cbc718a1
villanueva <villanueva.guerrero@outlook.com>
2024-03-03 11:32:37 -0500

    add: resources service

---
+++ MyPortfolio/ConfigureServices.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MyPortfolio.Services;
+
+namespace MyPortfolio
+{
+	public static class ConfigureServices
+	{
+		public static IServiceCollection AddConfigureServices(this IServiceCollection services, IConfiguration configuration, IWebAssemblyHostEnvironment hostEnvironment)
+		{
+			services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(hostEnvironment.BaseAddress) });
+
+			services.AddSingleton<ResourcesService>();
+
+			return services;
+		}
+	}
+}
--- MyPortfolio/Program.cs
+++ MyPortfolio/Program.cs
@@ -6,6 +6,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+var configuration = builder.Configuration;
+var env = builder.HostEnvironment;
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddConfigureServices(configuration, env);
 
 await builder.Build().RunAsync();
